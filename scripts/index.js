21c9e13d4db037e427e0a0868d83ae7ce9deb328
Alina Ivanova <abueva.lina@yandex.ru>
2020-10-05 23:39:11 +0400

    добавила аддаптивности

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,22 @@ const buttonOpenPopup = document.querySelector(".profile__edit-button");
 const buttonClosePopup = document.querySelector(".popup__close-icon");
 const popup = document.querySelector(".popup");
 const formElement = popup.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__name");
-let jobInput = document.querySelector(".popup__information");
+let nameInput = document.querySelector(".popup__information_name");
+let jobInput = document.querySelector(".popup__information_job");
 let profileTitle =  document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 
 
 function popupOpened () {
-popup.classList.add("popup_opened");
 nameInput.value = profileTitle.textContent;
 jobInput.value = profileSubtitle.textContent;
+popup.classList.add("popup_opened");
+
 }
 
 function popupClosed () {
 popup.classList.remove("popup_opened");
-// nameInput.value = profileTitle.textContent;
-// jobInput.value = profileSubtitle.textContent;
+
 }
 
 
@@ -28,9 +28,6 @@ buttonClosePopup.addEventListener("click", popupClosed);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    nameInput.value;
-    jobInput.value;
 
    profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
